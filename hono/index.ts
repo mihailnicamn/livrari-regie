0edb31d02e99f9447dd98d936ed92d0b6350034e
Mihail Nica <mihail.nica@gosocialdev.eu>
2023-11-06 20:31:19 +0200

    test

--- hono/index.ts
+++ hono/index.ts
@@ -3,12 +3,11 @@ import { cors } from 'hono/cors'
 import users from './routes/users'
 import markets from './routes/markets'
 import cart from './routes/cart'
+import { serveStatic } from 'hono/bun'
 const app = new Hono()
 app.use(cors())
-app.get('/', (c) => c.text('Hello Hono!'))
-
+app.use('/*', serveStatic({ root: './dist' }))
 app.route('/users', users)
 app.route('/markets', markets)
 app.route('/cart', cart)
-
 export default app
